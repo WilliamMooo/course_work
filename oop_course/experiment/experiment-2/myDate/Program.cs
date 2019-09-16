673a8cf4723a862aaeced74edde23fa8bd615f5f
WilliamMo <961017025@qq.com>
2019-09-16 15:30:02 +0800

    oop exp-2

---
+++ oop_course/experiment/experiment-2/myDate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
