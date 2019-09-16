ed29466f7b7bf9ac1b6d7885816bb25bdcba30a6
WilliamMo <961017025@qq.com>
2019-09-16 20:22:32 +0800

    exp-2

--- oop_course/experiment/experiment-2/dealSentence/Program.cs
+++ oop_course/experiment/experiment-2/dealSentence/Program.cs
@@ -4,9 +4,47 @@ namespace dealSentence
 {
     class Program
     {
+        class Sentence {
+            private string ret;
+            public void setArgs(string[] args) {
+                ret = args[args.Length-1];
+                for (int i = 0; i < args.Length-1; i++) deal(args[i]);
+            }
+            public void deal(string para) {
+                    if (para == "-w") {
+                        split();
+                    } else if (para == "-r") {
+                        reverse();
+                    } else if (para == "-u") {
+                        upper();
+                    }
+            }
+            public void split() {
+                ret = ret.Replace(" ", "\n");
+            }
+            public void reverse() {
+                char[] str = ret.ToCharArray();
+                Array.Reverse(str);
+                ret = new String(str);
+            }
+            public void upper() {
+                ret = ret.ToUpper();
+            }
+            public void output() {
+                Console.WriteLine(ret);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0 || args.Length > 4) {
+                Console.WriteLine("输入参数数量错误");
+            } else if (args.Length == 1) {
+                Console.WriteLine(args[0]);
+            } else {
+                Sentence newSen = new Sentence();
+                newSen.setArgs(args);
+                newSen.output();
+            }
         }
     }
 }
--- oop_course/experiment/experiment-2/myDate/Program.cs
+++ oop_course/experiment/experiment-2/myDate/Program.cs
@@ -4,9 +4,60 @@ namespace myDate
 {
     class Program
     {
+        class MyDate {
+            private int month;
+            private int day;
+            private int year;
+            public void setData(int m, int d, int y) {
+                if (checkMonth(m) && checkDay(m, d, y) && checkYear(y)) {
+                    month = m;
+                    day = d;
+                    year = y;
+                } else {
+                    Console.WriteLine("Wrong data!");
+                }
+            }
+            public void display() {
+                string m = month.ToString().PadLeft(2, '0');
+                string d = day.ToString().PadLeft(2, '0');
+                string y = year.ToString().PadLeft(4, '0');
+                Console.WriteLine("{0}/{1}/{2}", m, d, y);
+            }
+            protected bool checkMonth(int m) {
+                if (m<0 || m>12) return false;
+                else return true;
+            }
+            protected bool checkDay(int m, int d, int y) {
+                int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+                if (isLeapYear(y)) days[1] = 29;
+                if (d>0 && d <= days[m-1]) return true;
+                else return false;
+            }
+            protected bool checkYear(int y) {
+                if (y >= 0 && y < 10000) return true;
+                else return false;
+            }
+            protected bool isLeapYear(int y) {
+                if (y%400==0 || (y%4==0 && y%100!=0) ) return true;
+                else return false;
+            }
+            public MyDate() {
+                month = 01;
+                day = 01;
+                year = 1900;
+            }
+            public MyDate(int m, int d, int y) {
+                setData(m, d, y);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyDate oneDate = new MyDate();
+            oneDate.display();
+            oneDate.setData(2,29,1999);
+            oneDate.setData(2,28,1999); 
+            oneDate.display();
+            Console.WriteLine(oneDate);
         }
     }
 }
